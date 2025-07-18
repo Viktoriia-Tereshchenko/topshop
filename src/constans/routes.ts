16b5e80f3768f8a8aec453fca170d56ae74688e8
ViktoriiaTereshchenko <vika.s.tereshchenko@gmail.com>
2025-07-18 10:46:49 +0200

    start

---
+++ src/constans/routes.ts
@@ -0,0 +1,15 @@
+export const ROUTES = {
+  HOME: "/",
+  REGISTRATION: "/registration",
+  ABOUT: "/about",
+//   CONTACT: "/contact",
+  ACCOUNT: "/account",
+  PRODUCTS: "/products",
+  USERS: "/users",
+  USER: "/users/:userId",
+  DELETE_USER: "",
+  LOGIN: "/login",
+  ADD_PRODUCT: "/add-product",
+  DELETE_PRODUCT: "",
+} as const;
+
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,31 @@
+import { Routes, Route } from "react-router-dom";
+import { ROUTES } from "../constans/routes";
+import { MainLayout } from "../layouts/MainLayout";
+
+export default function AppRoutes() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          {/* <Route index element={<Home />} /> page  */}
+          {/* <Route path={ROUTES.REGISTRATION} element={<Registration />} />  page  */}
+          {/* <Route path={ROUTES.LOGIN} element={<Login />} />   page    */}
+          {/* <Route path={ROUTES.ACCOUNT} element={<Account />}> page  */}
+          {/* <Route path={ROUTES.ABOUT} element={<About />} /> page  */}
+
+          {/* <Route path={ROUTES.PRODUCTS} element={<ProductsPage/>} /> page  */}
+          {/* <Route path={ROUTES.PRODUCTS + "/:id"} element={<ProductPage />} />  page  */}
+          {/* <Route path={ROUTES.ADD_PRODUCT} element={<AddProduct />} /> page  */}
+          {/* <Route path={ROUTES.DELETE_PRODUCT} element={<DeleteProduct />} />   component / button       */}
+
+          {/* <Route path={ROUTES.USERS} element={<UsersPage />} /> page  */}
+          {/* <Route path={ROUTES.USER} element={<UserPage  />} /> page  */}
+          {/* <Route path={ROUTES.DELETE_USER} element={<DeleteUserPage/>} />  component / button     */}
+
+          {/* <Route path={ROUTES.CONTACT} element={<Contact />} />   -> footer*/}
+          {/* <Route path="*" element={<NotFound />} /> */}
+        </Route>
+      </Routes>
+    </>
+  );
+}

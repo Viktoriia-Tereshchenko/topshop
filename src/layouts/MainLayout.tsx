4d9822b2d5f58928645b1f097debe138db074acb
Andrey Shemet <an-shem@ukr.net>
2025-07-19 01:31:58 +0200

    fixed container

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -7,9 +7,9 @@ export const MainLayout = () => {
   return (
     <div className="flex flex-col min-h-screen">
       {/* навигация */}
-      <header className="fixed top-0 w-screen border-b-1 border-[#ececec] h-20 z-10 bg-secondary-100/95">
+      <header className="fixed top-0 w-full border-b-1 border-[#ececec] h-20 z-10 bg-secondary-100/95">
         <Container>
-          <div className="flex justify-between items-center size-full">
+          <div className="flex justify-between items-center  size-full">
             <Link to="/" className="font-secondary font-bold text-[26px]">
               <span className="text-accent">Top</span>Shop
             </Link>
@@ -25,7 +25,7 @@ export const MainLayout = () => {
         </Container>
       </main>
 
-      <footer className="min-h-24">
+      <footer className="bg-gray-900">
         <Container>
           <Footer />
         </Container>

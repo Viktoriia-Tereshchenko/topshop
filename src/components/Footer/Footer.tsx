c3639eff237c63ee30e5c6c71ea4d48dd950ed62
Eugen <eskhmelev@gmail.com>
2025-07-19 07:55:35 +0200

    footer-0755

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,30 +1,30 @@
 export default function Footer() {
   return (
     <div>
-      <footer className="bg-neutral-900 text-white py-10 px-4">
+      <footer className="bg-neutral-900  text-white py-10 px-4">
         <div className="max-w-7x1 mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 text-sm">
           <div>
-            <h2 className="text-white font-semibold mb-2">Компания</h2>
+            <h2 className="text-white font-semibold mb-2">Company</h2>
             <ul>
               <li>
                 <a href="/about" className="hover:underline text-neutral-500">
-                  О нас
+                  About
                 </a>
               </li>
               <li>
                 <a href="/team" className="hover:underline text-neutral-500">
-                  Команда
+                  Team
                 </a>
               </li>
               <li>
                 <a href="/careers" className="hover:underline text-neutral-500">
-                  Карьера
+                  Career
                 </a>
               </li>
             </ul>
           </div>
           <div>
-            <h2 className="text-white font-semibold mb-2">Помощь</h2>
+            <h2 className="text-white font-semibold mb-2">Help</h2>
             <ul>
               <li>
                 <a href="/faq" className="hover:underline text-neutral-500">
@@ -33,18 +33,18 @@ export default function Footer() {
               </li>
               <li>
                 <a href="/support" className="hover:underline text-neutral-500">
-                  Поддержка
+                  Help Center
                 </a>
               </li>
               <li>
                 <a href="/contact" className="hover:underline text-neutral-500">
-                  Контакты
+                  Contacts
                 </a>
               </li>
             </ul>
           </div>
           <div>
-            <h2 className="text-white font-semibold mb-2">Подпишитесь</h2>
+            <h2 className="text-white font-semibold mb-2">Subscribe</h2>
             <form className="flex flex-col">
               <input
                 type="email"
@@ -52,7 +52,7 @@ export default function Footer() {
                 className="p-2 rounded bg-gray-800 text-neutral-500 mb-2"
               />
               <button className="bg-blue-500 hover:bg-blue-700 px-4 py-2 rounded text-white">
-                Подписаться
+                Subscribe
               </button>
             </form>
           </div>
@@ -64,3 +64,5 @@ export default function Footer() {
     </div>
   );
 }
+
+

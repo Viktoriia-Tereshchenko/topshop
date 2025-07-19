d9dc61b488c14fdd57113af47911b59a4ba6e50c
Eugen <eskhmelev@gmail.com>
2025-07-19 19:59:54 +0200

    20250719-1959

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,20 +1,31 @@
 import { Routes, Route } from 'react-router-dom';
 import { ROUTES } from '../constants/routes';
 import { MainLayout } from '../layouts/MainLayout';
+import { AuthProvider } from '../providers/AuthProvider';
+import ProductsPage from '../pages/ProductsPage/ProductsPage';
+import { AddProduct } from '../components/AddProduct/AddProduct';
+import { AdminLayout } from '../pages/Admin/AdminLayout';
+import { AdminDashboard } from '../pages/Admin/AdminDashboard';
+import { ProductsManagement } from '../pages/Admin/ProductsManagement';
+import { CategoriesManagement } from '../pages/Admin/CategoriesManagement';
+import { UsersManagement } from '../pages/Admin/UsersManagement';
 import Logout from '../components/Logout/Logout';
 import Login from '../components/Login/Login';
 import Registration from '../components/Registration/Registration';
 import UsersPage from '../pages/UsersPage/UsersPage';
 import Account from '../components/Account/Account';
+import AboutPage from '../pages/AboutPage/AboutPage';
+import HomePage from '../pages/HomePage/HomePage';
+
 
 export default function AppRoutes() {
   return (
     <AuthProvider>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route index element={<div className="text-center text-gray-100 text-2xl font-bold mt-20">Welcome to TopShop!</div>} />
+          <Route index element={<HomePage />} /> page
           <Route path={ROUTES.REGISTRATION} element={<Registration />} />
-          {/* <Route path={ROUTES.ABOUT} element={<About />} /> */}
+          <Route path={ROUTES.ABOUT} element={<AboutPage />} />
           <Route path={ROUTES.ACCOUNT} element={<Account />} />
           <Route path={ROUTES.USERS} element={<UsersPage />} />
           {/* <Route path={ROUTES.USER} element={<User />} /> */}
@@ -24,7 +35,6 @@ export default function AppRoutes() {
           <Route path={ROUTES.PRODUCTS} element={<ProductsPage />} />
           <Route path={ROUTES.ADD_PRODUCT} element={<AddProduct />} />
         </Route>
-        
         {/* Admin Routes */}
         <Route path={ROUTES.ADMIN} element={<AdminLayout />}>
           <Route index element={<AdminDashboard />} />

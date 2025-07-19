72356766136d20e55b5dbc68715528f59133f9ef
Eugen <eskhmelev@gmail.com>
2025-07-19 17:42:57 +0200

    20250719-1742

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,14 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import { ROUTES } from '../constants/routes';
 import { MainLayout } from '../layouts/MainLayout';
-import { AuthProvider } from '../providers/AuthProvider';
-import ProductsPage from '../pages/ProductsPage/ProductsPage';
-import { AddProduct } from '../components/AddProduct/AddProduct';
-import { AdminDashboard } from '../pages/Admin/AdminDashboard';
-import { AdminLayout } from '../pages/Admin/AdminLayout';
-import { ProductsManagement } from '../pages/Admin/ProductsManagement';
-import { CategoriesManagement } from '../pages/Admin/CategoriesManagement';
-import { UsersManagement } from '../pages/Admin/UsersManagement';
 import Logout from '../components/Logout/Logout';
 import Login from '../components/Login/Login';
 import Registration from '../components/Registration/Registration';
@@ -16,6 +8,8 @@ import UsersPage from '../pages/UsersPage/UsersPage';
 import Account from '../components/Account/Account';
 import Team from '../pages/Team/Team';
 import ApplicationForm from '../pages/Careers/ApplicationForm';
+import AboutPage from '../pages/AboutPage/AboutPage';
+import HomePage from '../pages/HomePage/HomePage';
 
 export default function AppRoutes() {
   return (
@@ -36,6 +30,7 @@ export default function AppRoutes() {
         {/* <Route path={ROUTES.DELETE_USER} element={<DeleteUserPage/>} />  component / button     */}
         {/* <Route path={ROUTES.CONTACT} element={<Contact />} />   -> footer*/}
         <Route path="team" element={<Team />} />
+        <Route path="careers" element={<ApplicationForm />} />
       </Route>
     </Routes>
   );

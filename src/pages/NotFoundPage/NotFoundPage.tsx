3a51e178388b0e068e2175291c97bb1479f739cd
Andrey Shemet <an-shem@ukr.net>
2025-07-21 03:08:19 +0200

    fix NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 const NotFoundPage: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-50 p-4">
-      <img src="/img-404.png" alt="404 error illustration" className="w-screen/2 object-contain mb-8" />
+    <div className="flex flex-col items-center justify-center h-screen w-full bg-secondary-100 p-4">
+      <img src="/img-404.png" alt="404 error illustration" className="h-1/2 object-contain mb-8" />
 
       <h1 className="text-3xl md:text-4xl font-bold text-gray-800 mb-2 text-center">Looks like you’ve gotten lost.</h1>
 
@@ -12,7 +12,7 @@ const NotFoundPage: React.FC = () => {
 
       <Link
         to="/"
-        className="px-6 py-3 bg-accent text-white rounded-lg font-medium shadow-md hover:shadow-lg transition"
+        className="px-6 py-3 bg-[#afb1b8] hover:bg-accent text-white rounded-lg font-medium shadow-md hover:shadow-lg transition"
       >
         Home
       </Link>
